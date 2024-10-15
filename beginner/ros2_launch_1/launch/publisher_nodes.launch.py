1a20527a1c4c5c4f72da4a5a55b3d2fe8816d498
s-kitajima <s-kitajima@esol.co.jp>
2024-10-15 19:26:33 +0900

    Update the license description

--- beginner/ros2_launch_1/launch/publisher_nodes.launch.py
+++ beginner/ros2_launch_1/launch/publisher_nodes.launch.py
@@ -1,6 +1,5 @@
 #
-#  Copyright (C) 2023 eSOL Co.,Ltd.
-#  All rights reserved.
+#  Copyright (C) 2023 eSOL Co.,Ltd. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
